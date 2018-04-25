8bb4787decf47fedfb8a1c5e93ad82409aa68aad
Pedro Gomes <37028775+pedro-gomes-92@users.noreply.github.com>
2018-04-25 21:10:31 +0100

    Created category component

---
+++ src/components/category/category.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { BindableComponent } from '../bindable/bindable.component';
+
+@Component({
+  selector: 'category',
+  templateUrl: 'category.component.html'
+})
+export class CategoryComponent extends BindableComponent {
+
+}
