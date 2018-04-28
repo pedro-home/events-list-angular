1f0aedcd31c4158722f1398503ca63003763b407
Pedro Gomes <37028775+pedro-gomes-92@users.noreply.github.com>
2018-04-28 14:33:11 +0100

    Support of status on category

--- src/components/category/category.component.ts
+++ src/components/category/category.component.ts
@@ -7,4 +7,41 @@ import { BindableComponent } from '../bindable/bindable.component';
 })
 export class CategoryComponent extends BindableComponent {
 
+  activeStatusConfig: Array<Object>;
+
+  public constructor()
+  {
+    super();
+    this.activeStatusConfig = [
+      {
+        type: 'going',
+        color: 'primary',
+        label: 'Going',
+        icon: 'checkmark'
+      },
+      {
+        type: 'ignore',
+        color: 'danger',
+        label: 'Ignore',
+        icon: 'close'
+      }
+    ];
+  }
+
+  changeStatus(id: string, activeStatus: string, status: string)
+  {
+    let nextStatus: string;
+    switch (status)
+    {
+      case 'ignore':
+      case 'going':
+        nextStatus = 'none';
+        break;
+      case 'none':
+        nextStatus = activeStatus;
+    }
+
+    this.data.events.find(val => val.id === id)
+    .status = nextStatus;
+  }
 }
