0f84e713930cc4a6aa2c3e60b9f47424754eb5c3
Pedro Gomes <37028775+pedro-gomes-92@users.noreply.github.com>
2018-04-30 00:47:30 +0100

    Styled voters component

--- src/components/voters/voters.component.ts
+++ src/components/voters/voters.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BindableComponent } from '../bindable/bindable.component';
+import { UtilsService } from '../../services/utils.service';
 
 @Component({
   selector: 'voters',
-  templateUrl: 'voters.component.html'
+  templateUrl: 'voters.component.html',
+  providers: [UtilsService]
 })
-export class VotersComponent extends BindableComponent {
+export class VotersComponent extends BindableComponent implements OnInit {
+
+  @Input('maximumNumber')
+  maxNumber: number;
+
+  missNumber: number;
+
+  public constructor(private utils: UtilsService) {
+    super();
+    this.missNumber = 0;
+
+    // TODO: Calculate maxNumber through available space
+    this.maxNumber = 10;
+  }
+
+  public ngOnInit(): void {
+    this.limitVoters();
+  }
+
+  private limitVoters(): void {
+    this.missNumber = this.data.length - this.maxNumber;
+    this.data = this.utils.clone(this.data).slice(0, this.maxNumber);
+  }
 
 }
